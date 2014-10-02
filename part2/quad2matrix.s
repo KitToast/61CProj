83ebde4ae01fa381283a6d5ab3f824d0263884f7
jongyoonl <jylee1307@gmail.com>
2014-10-01 19:28:00 -0700

    not working

--- part2/quad2matrix.s
+++ part2/quad2matrix.s
@@ -1,6 +1,6 @@
 .text
 
-# Decodes a quadtree to the original matrix
+# Decodes a quadtree to the original matrix -Replicate that is used to complete 
 #
 # Arguments:
 #     quadtree (qNode*)
@@ -17,58 +17,49 @@
 #         qNode *child_NW, *child_NE, *child_SE, *child_SW; 20 24 28 32 
 #     }
 
-# Assume that a qNode has size 4 * 9 = 36 bytes
+# Assume that a qNode has size 4 * 9 = 36 bytes. Further assume that the temporary variables are not stored across jar
 
 quad2matrix:
 	
-	# allocate for quadtree
-	addiu $sp, $sp, -36
-	sw $s0, 0($sp)
-	lw $s0, 36($sp)
+	# load quadtree variables into stack
+	addiu $sp, $sp, -4
+	
+	sw $ra, 0($sp) # to store $ra
 	
 	# we play with $t0, a copy of $s0
 	move $t0, $s0
-	
-Recurse: 
-	
-	# preparing for double loop
-	li $t8, 0 # i = 0;
-	li $t9, 0 # j = 0; 
-	
+		
 	sw $t1, 0($t0) # store leaf in $t1
-	
 	bne $t1, $zero, Leaf # if the value leaf is not zero, it must be one, hence it must be a leaf
-	beq $t1, $zero, Else # otherwise, go to else
 	
-Else: 
 	# allocation for the children
-	addiu $sp, $sp, -36
 	
-	sw $t0, 0($sp) # save $t0 so we can return to it
+	addiu $sp, $sp, -8 #Store oringal pointer to parent quadtree struct
+	sw $s0, 0($sp)
+	sw $t0, 4($sp) #store $t0 to keep oringal pointer
 	
 	# NW
-	lw $t0, 36($sp)
-	lw $t0, 20($t0)
-	j Recurse
+	lw $s0, 20($t0) #Pointer to NW
+	jal quad2matrix
 	
-	#NE
-	lw $t0, 0($sp)
-	lw $t0, 24($t0)
-	j Recurse
+	# NE
+	lw $t0, 4($sp)
+	lw $s0, 24($t0) #Pointer to NE
+	jal quad2matrix
 	
-	#SE
-	lw $t0, 0($sp)
-	lw $t0, 28($t0)
-	j Recurse
+	# SE
+	lw $t0, 4($sp)
+	lw $s0, 28($t0) #Pointer to SE
+	jal quad2matrix
 	
-	#SW
-	lw $t0, 0($sp)
-	lw $t0, 32($t0)
-	j Recurse
+	# SW
+	lw $t0, 4($sp)
+	lw $s0, 32($t0) #Pointer to SW
+	jal quad2matrix
 	
-	# free 
-	lw $t0, 0($sp)
-	addiu $sp, $sp, 36
+	# free
+	lw $s0, 0($sp)
+	addiu $sp, $sp, 8
 	
 	j Exit
 
@@ -81,43 +72,22 @@ Leaf: # t0 points to the node of the leaf. Fills the matrix pointer.
 	lw $t5, 16($t0) # store gray_value in $t5
 	li $t7, 0       # $t7 will store our current offset
 	
-	mult
-	div 
+	mult $s2, $t4
+	mflo $t7
+	addu $t7, $t7, $t3 #Calculate offset
+	addu $t6, $s1, $t7 #t6 is the offset from beginning of matrix
 	
-	beq $t8, $t2, ExitRow # if i == width, break
-	beq $t9, $t2, ExitCol # if j == height, break
-	
-	
-
-ExitRow: 
-
-ExitCol: 
-
+OuterLoop:	beq $t8, $t2, Exit # if i == width, break
+InnerLoop:	beq $t9, $t2, ExitCol # if j == height, break
+		sb $t5, 0($t6)
+		addiu $t6, $t6, 1 #increment j 
+		j InnerLoop	
+ExitCol: 	addiu $t8, $t8, 1 #increment i
+		addu $t7, $t7, $s2 #Go to next row 
+		move $t6, $t7
+		j OuterLoop
 Exit: 
-	lw $s0, 0($sp)
-	addiu $sp, $sp, 36
+	sw $ra, 0($sp)
+	addiu $sp, $sp, 4
 	move $v0, $s1
 	jr $ra
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	blt 
-	
-	
-	
-	
-	
-	
-	
