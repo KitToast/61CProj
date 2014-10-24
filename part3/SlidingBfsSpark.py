230ce586b241d9ded6e226a09c966b372e5ace53
Wir3dToast <edbeta12@gmail.com>
2014-10-23 22:07:49 -0700

    Works under time constraints for 2x2 and 3x3. More optimization definitely needed for 5x2. Going to try to use partitions

--- part3/SlidingBfsSpark.py
+++ part3/SlidingBfsSpark.py
@@ -4,10 +4,14 @@ import Sliding, argparse
 def bfs_map(board):
     if (board not in already_seen_set_broadcast.value):
         return ( level, [board] )
-
+    return (level, [])
 
 def bfs_reduce(board1, board2):
-    return board1 + board2 #Combine sets  
+    if(len(board2) == 1 and board2[0] in board1): #Early duplicate checking when lists are only length one. Prevents further duplicate checking in iterative portion
+        return board1
+    elif(len(board1) == 1 and board1[0] in board2):
+        return board2
+    return board1 + board2  
 
 def solve_sliding_puzzle(master, output, height, width):
     """
@@ -29,9 +33,9 @@ def solve_sliding_puzzle(master, output, height, width):
     WIDTH=width
     level = 0 # this "constant" will change, but it remains constant for every MapReduce job
     
+    firstlevel = True;
     # The solution configuration for this sliding puzzle. You will begin exploring the tree from this node
-    sol = list(Sliding.solution(WIDTH, HEIGHT))
-
+    sol = [Sliding.solution(WIDTH, HEIGHT)]
     #Create initial RDD from entry solution point
     sol_rdd = sc.parallelize(sol)
     #Initialization
@@ -39,19 +43,31 @@ def solve_sliding_puzzle(master, output, height, width):
     already_seen_set_broadcast = sc.broadcast(already_seen_set)
     current_lvl_dataset = sol_rdd
 
-    while level < 2: #Testing for only level 0 and 1. Onward will require a helper function dealing with Calling MapReduce with Sliding.children 
+    while True: #Testing for only level 0 and 1. Onward will require a helper function dealing with Calling MapReduce with Sliding.children 
         current_lvl_result = current_lvl_dataset.map(bfs_map).reduceByKey(bfs_reduce)
         current_lvl_list = current_lvl_result.collect()
 
-        for result in current_lvl_list: #Bottleneck?
-            output(result)
+        for result in current_lvl_list: #Bottleneck and change to correct format
+            for board in current_lvl_list[0][1]:
+                output(str(result[0]) + " " + str(board))
+        
+        if(not current_lvl_list):
+            break
 
-        already_seen_set.union(set(current_lvl_list[0][1])) #Update seen set
+        list_of_unique_children = current_lvl_list[0]
+
+        if(len(list_of_unique_children[1]) == 1 and not firstlevel):
+            break
+        already_seen_set = already_seen_set.union(set(list_of_unique_children[1])) #Update seen set
+        already_seen_set_broadcast.unpersist(blocking = True) 
         already_seen_set_broadcast = sc.broadcast(already_seen_set) #update broadcast
-   
-        children_boards = Sliding.children(HEIGHT, WIDTH , current_lvl_list[0][1]) #May have to move this to mapping portion or somehow make it parallel. Another MapReduce?
+
+        children_boards = []
+        for board in list_of_unique_children[1]:
+            children_boards.extend(Sliding.children(WIDTH, HEIGHT , board)) #May have to move this to mapping portion or somehow make it parallel. Another MapReduce?
         current_lvl_dataset = sc.parallelize(children_boards)
         level += 1
+        firstlevel = False
 
     sc.stop()
 
