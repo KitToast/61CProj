d741cc156f31f78ae715e8dc8428a2453d1d32fd
Jongyoon_Lee <cs61c-afq@s275-20.CS.Berkeley.EDU>
2014-10-27 21:08:59 -0700

    Commit

--- part3/SlidingBfsSpark.py
+++ part3/SlidingBfsSpark.py
@@ -7,10 +7,13 @@ def bfs_map(board_tup):
     return (level, [])
 
 def bfs_reduce(board1, board2):
-    return board1 + board2  
+    return board1 + board2
 
 def children_map(child):
     return (level, child)
+
+def children_flatMap(board):
+    return Sliding.children(WIDTH, HEIGHT, board)
 
 def solve_sliding_puzzle(master, output, height, width):
     """
@@ -25,14 +28,13 @@ def solve_sliding_puzzle(master, output, height, width):
 
     # Global constants that will be shared across all map and reduce instances.
     # You can also reference these in any helper functions you write.
-    global HEIGHT, WIDTH, level, already_seen_set_broadcast
+    global HEIGHT, WIDTH, level, already_seen_set_broadcast, four
 
     # Initialize global constants
     HEIGHT=height
     WIDTH=width
     level = 0 # this "constant" will change, but it remains constant for every MapReduce job
     
-    firstlevel = True;
     # The solution configuration for this sliding puzzle. You will begin exploring the tree from this node
     sol = [Sliding.solution(WIDTH, HEIGHT)]
     #Create initial RDD from entry solution point
@@ -41,35 +43,52 @@ def solve_sliding_puzzle(master, output, height, width):
     already_seen_set = set() #Solution is already seen
     already_seen_set_broadcast = sc.broadcast(already_seen_set)
     current_lvl_dataset = sol_rdd
+    
+    current_lvl_result = current_lvl_dataset.map(bfs_map).reduceByKey(bfs_reduce) #reduce could happen a lot fewer times
+    current_lvl_list = current_lvl_result.collect() #collect is also an action, which makes it bad. 
 
-    while True: #Testing for only level 0 and 1. Onward will require a helper function dealing with Calling MapReduce with Sliding.children 
-        current_lvl_result = current_lvl_dataset.map(bfs_map).reduceByKey(bfs_reduce)
-        current_lvl_list = current_lvl_result.collect()
+    for result in current_lvl_list: #Bottleneck and change to correct format
+        for board in current_lvl_list[0][1]:
+            output(str(result[0]) + " " + str(board))
+    list_of_unique_children = current_lvl_list[0]
+    
+    already_seen_set = already_seen_set.union(set(list_of_unique_children[1])) #Update seen set
+    already_seen_set_broadcast.unpersist(blocking = True) 
+    already_seen_set_broadcast = sc.broadcast(already_seen_set) #update broadcast
+    
+    children_boards = []
+    for board in list_of_unique_children[1]:
+        children_boards.extend(Sliding.children(WIDTH, HEIGHT, board))
+    current_lvl_dataset = sc.parallelize(children_boards).distinct().map(children_map)
+    current_lvl_dataset = current_lvl_dataset.partitionBy(8)
+    level += 1
+    
+    while True:
+
+        current_lvl_result = current_lvl_dataset.map(bfs_map).reduceByKey(bfs_reduce) #reduce could happen a lot fewer times
+    	current_lvl_list = current_lvl_result.collect() #collect is also an action, which makes it bad.
+
+        if (not current_lvl_list):
+            break
 
         for result in current_lvl_list: #Bottleneck and change to correct format
             for board in current_lvl_list[0][1]:
                 output(str(result[0]) + " " + str(board))
-        
-        if(not current_lvl_list):
-            break
 
         list_of_unique_children = current_lvl_list[0]
 
-        if(len(list_of_unique_children[1]) == 1 and not firstlevel):
+        if(len(list_of_unique_children[1]) == 1):
             break
 
         already_seen_set = already_seen_set.union(set(list_of_unique_children[1])) #Update seen set
         already_seen_set_broadcast.unpersist(blocking = True) 
         already_seen_set_broadcast = sc.broadcast(already_seen_set) #update broadcast
 
-        children_boards = []
-        for board in list_of_unique_children[1]:
-            children_boards.extend(Sliding.children(WIDTH, HEIGHT, board)) #May have to move this to mapping portion or somehow make it parallel. Another MapReduce?
-        current_lvl_dataset = sc.parallelize(children_boards).distinct().map(children_map)
-        if (level % 8) == 0:
-            current_lvl_dataset = current_lvl_dataset.partitionBy(16)
         level += 1
-        firstlevel = False
+
+	current_lvl_dataset = sc.parallelize(list_of_unique_children[1]).flatMap(children_flatMap).distinct().map(children_map)
+        if (level % 8) == 0:
+            current_lvl_dataset = current_lvl_dataset.partitionBy(8)
 
     sc.stop()
 
