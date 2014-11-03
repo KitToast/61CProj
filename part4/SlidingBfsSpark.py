12e45f7c60fdb57d195b707283e97f93746ea236
Wir3dToast <edbeta12@gmail.com>
2014-11-02 16:10:37 -0800

    New code for Amazon ec2

--- part4/SlidingBfsSpark.py
+++ part4/SlidingBfsSpark.py
@@ -1,17 +1,68 @@
 from pyspark import SparkContext
 import Sliding, argparse
 
-def solve_puzzle(master, output, height, width, slaves):
-    global HEIGHT, WIDTH, level
-    HEIGHT=height
-    WIDTH=width
-    level = 0
 
+def bfs_reduce(level1, level2):    
+    return min(level1, level2)
+
+def bfs_flatmap(board):
+    if board[1] == (level - 1):
+        children_list = Sliding.children(WIDTH, HEIGHT, Sliding.hash_to_board(WIDTH, HEIGHT, board[0]))
+	children_list = map(lambda board: Sliding.board_to_hash(WIDTH, HEIGHT, board), children_list) 
+        level_list = [level for _ in range(len(children_list))]
+        children_list = zip(children_list, level_list)
+        children_list.append(board)
+        return children_list
+    return [board]
+
+def flip_map(board):
+    return (board[1], board[0])
+
+def flip_reduce(board1, board2):
+    return board1
+
+def solve_puzzle(master, output, height, width, slaves):
+    """
+    Solves a sliding puzzle of the provided height and width.
+     master: specifies master url for the spark context
+     output: function that accepts string to write to the output file
+     height: height of puzzle
+     width: width of puzzle
+    """
+    # Set up the spark context. Use this to create your RDD
     sc = SparkContext(master, "python")
 
-    """ YOUR CODE HERE """
+    # Global constants that will be shared across all map and reduce instances.
+    # You can also reference these in any helper functions you write.
+    global HEIGHT, WIDTH, level
 
-    sc.stop()
+    # Initialize global constants
+    HEIGHT=height
+    WIDTH=width
+    level = 1 # this "constant" will change, but it remains constant for every MapReduce job
+
+    # The solution configuration for this sliding puzzle. You will begin exploring the tree from this node
+    sol = [ ( Sliding.board_to_hash( WIDTH, HEIGHT, Sliding.solution(WIDTH, HEIGHT) ), 0 ) ]
+    sol_rdd = sc.parallelize(sol)
+   
+    prev_lvl_count = 0
+    current_lvl_count = 1
+    current_lvl_rdd = sol_rdd
+
+    """ YOUR MAP REDUCE PROCESSING CODE HERE """
+    while prev_lvl_count != current_lvl_count:
+        current_lvl_rdd = current_lvl_rdd.flatMap(bfs_flatmap).reduceByKey(bfs_reduce)
+        prev_lvl_count = current_lvl_count
+        current_lvl_count = current_lvl_rdd.count()
+        level += 1
+        if level % 8 == 0:
+            current_lvl_rdd = current_lvl_rdd.partitionBy(PARTITION_COUNT) #change this to partition count later
+ 
+    """ YOUR OUTPUT CODE HERE """
+
+    current_lvl_rdd = current_lvl_rdd.map(flip_map).sortByKey()
+    current_lvl_rdd.coalesce(12).saveAsTextFile(output)
+
 
 
 
