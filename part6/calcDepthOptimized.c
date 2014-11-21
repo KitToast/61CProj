f257ad579a9e594e36d73ab926e216ba7d772102
Wir3dToast <edbeta12@gmail.com>
2014-11-20 23:31:15 -0800

    Starting copy that may pass

--- part6/calcDepthOptimized.c
+++ part6/calcDepthOptimized.c
@@ -35,101 +35,108 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
     int x_bound = imageWidth - featureWidth;
 
     memset(depth, 0, imageWidth * imageHeight * sizeof(float)); //Memset all of depth map to zero
-
-    for (int y = featureHeight; y < y_bound; y++)
+    #pragma omp parallel shared(imageWidth,imageHeight,maximumDisplacement,left,right)
     {
-
-        for (int x = featureWidth; x < x_bound; x++)
-        {
-
-            float minimumSquaredDifference = -1;
-            int minimumDy = 0;
-            int minimumDx = 0;
-
-            for (int dx = -maximumDisplacement; dx <= maximumDisplacement; dx++)
+      #pragma omp for
+     for (int y = featureHeight; y < y_bound; y++)
+     {
+            for (int x = featureWidth; x < x_bound; x++)
             {
-                for (int dy = -maximumDisplacement; dy <= maximumDisplacement; dy++)
-                {
-                    if (y + dy - featureHeight < 0 || y + dy + featureHeight >= imageHeight || x + dx - featureWidth < 0 || x + dx + featureWidth >= imageWidth)
+
+                float minimumSquaredDifference = -1;
+                int minimumDy = 0;
+                int minimumDx = 0;
+                
+                //#pragma omp parallel shared(x,y,imageWidth,imageHeight,maximumDisplacement,left,right)
+                //{
+                    for (int dx = -maximumDisplacement; dx <= maximumDisplacement; dx++)
                     {
-                        continue;
+                        for (int dy = -maximumDisplacement; dy <= maximumDisplacement; dy++)
+                        {
+                            if (y + dy - featureHeight < 0 || y + dy + featureHeight >= imageHeight || x + dx - featureWidth < 0 || x + dx + featureWidth >= imageWidth)
+                            {
+                                continue;
+                            }
+
+                            float squaredDifference = 0; //Remember to coalesce this in the future
+        		            float tailsquaredDifference = 0;
+                            __m128 msquared_difference = _mm_setzero_ps();
+                            int leftY;
+                            int rightY;
+                            int leftX;
+                            int rightX;
+                            float difference;
+
+                            for (int boxY = -featureHeight; boxY <= featureHeight; boxY++)
+                            {
+                                leftY = y + boxY;
+                                rightY = y + dy + boxY;
+                                int leftProductY = leftY * imageWidth;
+                                int rightProductY = rightY * imageWidth;
+                                for (int boxX = -featureWidth; boxX < sseBounds; boxX += 4)
+                                {
+                                    leftX = x + boxX;
+                                    rightX = x + dx + boxX;
+
+                                    __m128 left_image0 = _mm_loadu_ps(&(left[leftProductY + leftX]));
+                                    __m128 right_image0 = _mm_loadu_ps(&(right[rightProductY + rightX]));
+
+                                    __m128 difference0 = _mm_sub_ps(left_image0,right_image0);
+                                    msquared_difference  = _mm_add_ps(msquared_difference, _mm_mul_ps(difference0, difference0));
+                                }
+
+
+                                leftX = x + sseBounds;
+                                rightX = x + dx + sseBounds;
+                                //Tail Case
+                                if(tail_case == 1)
+                                {
+                                    difference = left[leftY * imageWidth + leftX] - right[rightY * imageWidth + rightX];
+                                    tailsquaredDifference += difference * difference;
+                                }
+                                else
+                                {
+                                    difference = left[leftY * imageWidth + leftX] - right[rightY * imageWidth + rightX];
+                                    tailsquaredDifference += difference * difference;
+
+                                    difference = left[leftY * imageWidth + leftX + 1] - right[rightY * imageWidth + rightX + 1];
+                                    tailsquaredDifference += difference * difference;
+
+                                    difference = left[leftY * imageWidth + leftX + 2] - right[rightY * imageWidth + rightX + 2];
+                                    tailsquaredDifference += difference * difference;
+                                }
+                            }
+
+                                float results[4];
+                                msquared_difference = _mm_hadd_ps(msquared_difference,msquared_difference);
+                                msquared_difference = _mm_hadd_ps(msquared_difference,msquared_difference);
+                                _mm_storeu_ps(results,msquared_difference);
+                                squaredDifference += results[0];
+                    			squaredDifference += tailsquaredDifference;
+
+                                if ((minimumSquaredDifference == -1) || 
+                                   ((minimumSquaredDifference == squaredDifference) && 
+                                   (displacementNaive(dx,dy) < displacementNaive(minimumDx, minimumDy))) || 
+                                   ((minimumSquaredDifference > squaredDifference) && 
+                                   (minimumSquaredDifference != squaredDifference)))
+                                {
+                                    minimumSquaredDifference = squaredDifference;
+                                    minimumDx = dx;
+                                    minimumDy = dy;
+                                }
+                        }
                     }
 
-                    float squaredDifference = 0; //Remember to coalesce this in the future
-		    float tailsquaredDifference = 0;
-                    __m128 msquared_difference = _mm_setzero_ps();
-                    int leftY;
-                    int rightY;
-                    int leftX;
-                    int rightX;
+                //}
 
-                    for (int boxY = -featureHeight; boxY <= featureHeight; boxY++)
+                    if (minimumSquaredDifference != -1)
                     {
-                        leftY = y + boxY;
-                        rightY = y + dy + boxY;
-                        int leftProductY = leftY * imageWidth;
-                        int rightProductY = rightY * imageWidth;
-                        for (int boxX = -featureWidth; boxX < sseBounds; boxX += 4)
+                        if (maximumDisplacement != 0)
                         {
-                            leftX = x + boxX;
-                            rightX = x + dx + boxX;
-
-                            __m128 left_image0 = _mm_loadu_ps(&(left[leftProductY + leftX]));
-                            __m128 right_image0 = _mm_loadu_ps(&(right[rightProductY + rightX]));
-
-                            __m128 difference0 = _mm_sub_ps(left_image0,right_image0);
-                            msquared_difference  = _mm_add_ps(msquared_difference, _mm_mul_ps(difference0, difference0));
+                            depth[y * imageWidth + x] = displacementNaive(minimumDx, minimumDy);
                         }
-
-
-                        leftX = x + sseBounds;
-                        rightX = x + dx + sseBounds;
-                        //Tail Case
-                        if(tail_case == 1)
-                        {
-                            float difference = left[leftY * imageWidth + leftX] - right[rightY * imageWidth + rightX];
-                            tailsquaredDifference += difference * difference;
-                        }
-                        else
-                        {
-                            float difference = left[leftY * imageWidth + leftX] - right[rightY * imageWidth + rightX];
-                            tailsquaredDifference += difference * difference;
-
-                            difference = left[leftY * imageWidth + leftX + 1] - right[rightY * imageWidth + rightX + 1];
-                            tailsquaredDifference += difference * difference;
-
-                            difference = left[leftY * imageWidth + leftX + 2] - right[rightY * imageWidth + rightX + 2];
-                            tailsquaredDifference += difference * difference;
-                        }
-                    }
-
-                        float results[4];
-                        msquared_difference = _mm_hadd_ps(msquared_difference,msquared_difference);
-                        msquared_difference = _mm_hadd_ps(msquared_difference,msquared_difference);
-                        _mm_storeu_ps(results,msquared_difference);
-                        squaredDifference += results[0];
-			squaredDifference += tailsquaredDifference;
-
-                    if ((minimumSquaredDifference == -1) || 
-                       ((minimumSquaredDifference == squaredDifference) && 
-                       (displacementNaive(dx,dy) < displacementNaive(minimumDx, minimumDy))) || 
-                       ((minimumSquaredDifference > squaredDifference) && 
-                       (minimumSquaredDifference != squaredDifference)))
-                    {
-                        minimumSquaredDifference = squaredDifference;
-                        minimumDx = dx;
-                        minimumDy = dy;
-                    }
-                }
+                    }   
             }
-
-            if (minimumSquaredDifference != -1)
-            {
-                if (maximumDisplacement != 0)
-                {
-                    depth[y * imageWidth + x] = displacementNaive(minimumDx, minimumDy);
-                }
-            }
-        }
+      }
     }
 }
